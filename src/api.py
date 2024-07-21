d08737a6afa146b39c7f5875884cce80afbcaa2c
Jason Knoll <jasonknoll98@aol.com>
2024-07-21 11:28:15 -0400

    added firestore and test endpoint

--- src/api.py
+++ src/api.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+auth_path = os.getenv("AUTH_PATH")
+
+creds = credentials.Certificate(auth_path)
+firebase_admin.initialize_app(creds)
+db = firestore.client()
+
 @app.get("/")
-def index():
+async def index():
     return {"out":"Hello World"}
+
+@app.get("/test")
+async def test():
+    document = db.collection("songs").document("test")
+    doc = document.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        return {"error" : "you idiot"}
 
 
